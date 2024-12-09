9f361e3adbf2b773b279e8920c1439eac9011011
Muhammad Owais <rahneverd@gmail.com>
2024-12-09 14:30:23 +0500

    added album and photo resource

--- ossh-cloud-be/src/app.module.ts
+++ ossh-cloud-be/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { PhotoModule } from './photo/photo.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule, PhotoModule],
   controllers: [AppController],
   providers: [AppService],
 })
