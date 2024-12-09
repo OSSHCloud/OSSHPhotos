f46f9c74674512d406b320eb9ee4796c543fd876
Muhammad Owais <rahneverd@gmail.com>
2024-12-09 14:36:36 +0500

    added list-of-values and lov-category resources

--- ossh-cloud-be/src/app.module.ts
+++ ossh-cloud-be/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './album/album.module';
 import { PhotoModule } from './photo/photo.module';
+import { ListOfValuesModule } from './list-of-values/list-of-values.module';
+import { LovCategoryModule } from './lov-category/lov-category.module';
 
 @Module({
-  imports: [AlbumModule, PhotoModule],
+  imports: [AlbumModule, PhotoModule, ListOfValuesModule, LovCategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
